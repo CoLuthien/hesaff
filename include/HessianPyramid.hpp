8fea88bc83ce1d0b5e01b52d4db842d36159bc2f
garam <garam.ttree@gmail.com>
2023-12-04 08:31:44 +0900

    파라미터 기본값 변경

--- include/HessianPyramid.hpp
+++ include/HessianPyramid.hpp
@@ -9,9 +9,9 @@ namespace ha
 struct HessianResponsePyramidParams
 {
 public:
-    int   numOctaves   = 5;   // number of half scaled image for octaves
-    int   numLayers    = 3;   // amount of gaussian blurred image for each Hessian Octave
-    float initialSigma = 1.6; // amount of smoothing applied to the initial level of first octave
+    int   numOctaves   = 3; // number of half scaled image for octaves
+    int   numLayers    = 3; // amount of gaussian blurred image for each Hessian Octave
+    float initialSigma = 3.; // amount of smoothing applied to the initial level of first octave
 };
 
 class HA_API HessianResponseOctave
